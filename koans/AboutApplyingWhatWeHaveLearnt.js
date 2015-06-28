f11248849f227ad7f83fce8005e23c20f90bd1fc
Eric <hoang.e.nguyen@gmail.com>
2015-06-28 17:17:28 -0500

    Completed "allergic to nuts and hate mushrooms" .filter() and .all() exercise.

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -30,15 +30,22 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
       var productsICanEat = [];
 
       /* solve using filter() & all() / any() */
+      _(products).filter(function(pizza){
+        if(!pizza.containsNuts){
+          if(_(pizza.ingredients).all(function(x){return x!=="mushrooms"})){
+            productsICanEat.push(pizza)}
+        }
+      })
+      
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
@@ -52,11 +59,11 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
     
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+    var sum =  /* try chaining range() and reduce() */
 
     expect(233168).toBe(FILL_ME_IN);
   });
@@ -78,7 +85,7 @@ describe("About Applying What We Have Learnt", function() {
     var ingredientCount = { "{ingredient name}": 0 };
 
     /* chain() together map(), flatten() and reduce() */
-
+   
     expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
   });
 
